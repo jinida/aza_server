949b0031816529e46b9828fc7c0ed7ddbfb2add1
jini <qkqh9912@gmail.com>
2023-02-06 07:57:51 +0000

    Update Code

--- src/server.cpp
+++ src/server.cpp
@@ -1,4 +1,3 @@
-#include <conio.h>
 #include "server.hpp"
 
 void Server::do_accept()
@@ -8,52 +7,15 @@ void Server::do_accept()
         {
             if (!ec)
             {
-                std::make_shared<Session>(std::move(socket), sessionNum++)->start();
+                auto pSession = std::make_shared<Session>(std::move(socket));
+                pLogger_queue_->push(Logger(pLogger_, "Create Session", Logger::LoggerType::SERVER, Logger::LoggerStatus::PROCESSED, Logger::LoggerInfo::INFO));
+                Event evt(sessionNum++, 0, 0, pSession);
+                pEvt_queue_->push(evt);
+                Logger log(pLogger_, Logger::LoggerType::SERVER, Logger::LoggerStatus::GENEARTED, Logger::LoggerInfo::INFO);
+                pLogger_queue_->push(log);
+                
+                pSession->start(&evt, &log);
             }
             do_accept();
         });
-}
-
-void Session::do_read_header()
-{
-    auto self(shared_from_this());
-    auto f = [this, self](boost::system::error_code ec, std::size_t length)
-        {
-            if (!ec && read_event.getEventPacket().decodeHeader())
-            {
-                do_read_body();
-            }
-        };
-    boost::asio::async_read(socket_, boost::asio::buffer(read_event.getEventPacket().data(), LEN_HEADER), f)
-}
-
-void Session::do_read_body()
-{
-    auto self(shared_from_this());
-    auto f = [this, self](boost::system::error_code ec, std::size_t length)
-        {
-            if (!ec)
-            {   
-                do_read_header();
-            }
-        };
-    boost::asio::async_read(socket_, boost::asio::buffer(read_event.getEventPacket().body(), 
-        read_event.getEventPacket().body_length()), f);
-}
-
-void Session::do_write()
-{
-    auto self(shared_from_this());
-    auto f = [this, self](boost::system::error_code ec, std::size_t length)
-        {
-            if (!ec)
-            {
-                write_events.pop_front();
-                if (!write_events.empty())
-                {
-                    do_write();
-                }
-            }
-        };
-    boost::asio::async_write(socket_, boost::asio::buffer(write_events.top().getEventPacket().getData(), write_events.top().getEventPacket().getLen()), f);
 }
